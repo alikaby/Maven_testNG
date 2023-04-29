731996fce81a56e74dd6d792038ab8bc4f867ced
Александра Быстрова <saobyjob90@gmail.com>
2023-04-29 21:59:11 +0300

    Написала тест для метода isTeenager, использовала DataProvider для создания разнообразных наборов данных, описала ошибки в реализации метода isTeenager

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,8 +1,38 @@
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class PersonTest {
-    @Test(dataProvider = "sumDataProvider")
-    public void testSum(int arg1, int arg2, int expectedSum)
-    {
+    @DataProvider(name = "ageDataProvider")
+    Object[][] ageDataProvider() {
+        return new Object[][] {
+                {12, false},
+                {13, true},
+                {14, true},
+                {15, true},
+                {16, true},
+                {17, true},
+                {18, true},
+                {19, true},
+                {20, false},
+                {0, false}
+        };
     }
+    @Test(dataProvider = "ageDataProvider")
+    public void testAge(int printAge, boolean isTeenager)
+    {
+        assertEquals(Person.isTeenager(printAge),isTeenager);
+    }
+
+   @Test public void testAge() {
+       assertEquals(true, Person.isTeenager(18));
+       assertEquals(true, Person.isTeenager(15));
+       assertEquals(true, Person.isTeenager(13));
+       assertEquals(false, Person.isTeenager(12));
+       assertEquals(false, Person.isTeenager(4));
+       assertEquals(false, Person.isTeenager(-10));
+       assertEquals(false, Person.isTeenager(20));
+       assertEquals(true, Person.isTeenager(19));
+   }
 }
